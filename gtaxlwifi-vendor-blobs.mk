e394a540d96b2425d14f2e995fccb1e170b63f97
Valera1978 <valera1978@tut.by>
2017-12-27 18:51:35 +0300

    update libsec-ril

--- gtaxlwifi-vendor-blobs.mk
+++ gtaxlwifi-vendor-blobs.mk
@@ -96,6 +96,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxlwifi/proprietary/lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/libhwjpeg.so:system/lib/libhwjpeg.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/libpreprocessing_nxp.so:system/lib/libpreprocessing_nxp.so \
+    vendor/samsung/gtaxlwifi/proprietary/lib/libprotobuf-cpp-fooo.so:system/lib/libprotobuf-cpp-fooo.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/libSamsungPostProcess.so:system/lib/libSamsungPostProcess.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/libSamsungPostProcessConvertor.so:system/lib/libSamsungPostProcessConvertor.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/libSamsungPreProcess.so:system/lib/libSamsungPreProcess.so \
@@ -145,9 +146,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxlwifi/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/gtaxlwifi/proprietary/lib/librilutils.so:system/lib/librilutils.so \
-
-PRODUCT_COPY_FILES += \
-    vendor/samsung/gtaxllte/proprietary/vendor/lib/libprotobuf-cpp-shim.so:system/vendor/lib/libprotobuf-cpp-shim.so
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxlwifi/proprietary/bin/prepare_param.sh:system/bin/prepare_param.sh
